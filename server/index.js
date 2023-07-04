3653f4138434142561934633d26640479feb2160
Dipashree <dipashree@Dipashrees-MacBook-Air.local>
2023-07-04 21:59:02 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,19 @@
+// Node server which wil handle socket io connections
+const io= require('socket.io')(8000)
+const users = {};
+io.on('connection', socket => {
+    socket.on('new-user-joined', name => 
+    {
+        users[socket.id] = name;
+socket.broadcast.emit('user-joined', name)
+    });
+
+    socket.on('send',message=>{
+        socket.broadcast.emit('receive',{message: message, name:users[socket.id]} )
+    });
+    
+    socket.on('disconnect', msg => {
+        socket.broadcast.emit(`'left', ${msg} users[socket.id]`)
+delete users[socket.id];
+    })
+});
